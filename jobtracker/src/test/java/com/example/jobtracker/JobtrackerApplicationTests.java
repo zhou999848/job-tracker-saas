dcdb1089294e0bf9ee2965e506cade5a85b44e7e
zhou999848 <zhouzhihao378@gmail.com>
2025-07-12 10:34:04 +0900

    feat:add HelloController with /version/api

---
+++ jobtracker/src/test/java/com/example/jobtracker/JobtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
