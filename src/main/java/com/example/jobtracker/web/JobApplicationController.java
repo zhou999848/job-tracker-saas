9f39a033a86c27cc01eff347e71226cf2ebfbe47
zhou999848 <zhouzhihao378@gmail.com>
2025-07-18 17:09:54 +0900

    feat: add file upload endpoint

--- src/main/java/com/example/jobtracker/web/JobApplicationController.java
+++ src/main/java/com/example/jobtracker/web/JobApplicationController.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;//fenyepaixu
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 @RestController
 @RequestMapping("/api/jobs")
 public class JobApplicationController {
@@ -22,8 +26,33 @@ public class JobApplicationController {
     public void create(@RequestBody @Valid  JobApplicationDto dto) {
         service.save(dto);
     }
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                return "文件不能为空";
+            }
 
-    @GetMapping
+            String contentType = file.getContentType();
+            if (!contentType.equals("application/pdf")
+                    && !contentType.equals("image/jpeg")
+                    && !contentType.equals("image/png")) {
+                return "只支持 PDF/JPG/PNG 文件";
+            }
+
+            String path = "uploads/" + file.getOriginalFilename();
+            File dest = new File(path);
+            file.transferTo(dest);
+
+            return "上传成功: " + path;
+        } catch (Exception e) {
+            e.printStackTrace(); // 打印错误堆栈
+            return "上传失败: " + e.getMessage();
+        }
+    }
+
+
+    @GetMapping("/list")
     public List<JobApplicationDto> list() {
         return service.findAll();
     }
@@ -35,6 +64,7 @@ public class JobApplicationController {
     ) {
         return service.findAll(page, size, sortBy, direction);
     }
+   @GetMapping("/search")
     public Page<JobApplicationDto> search(@RequestParam String keyword,
                                           @RequestParam(defaultValue = "0") int page,
                                           @RequestParam(defaultValue = "5") int size) {
